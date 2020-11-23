b60cf5ae61a2d1d8704999e9da8dbf3d39001bcf
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-11-23 13:23:33 +0100

    11/23 last cleaning

--- src/js/views/addNewView.js
+++ src/js/views/addNewView.js
@@ -3,27 +3,15 @@ import 'regenerator-runtime/runtime';
 
 import { elements, clean } from '../helper.js';
 
-export const getInput = () => {
-  let name = elements.nameInput.value;
-  let age = elements.ageInput.value;
-
-  return { name, age };
-};
-
 export const showNotification = (status, newPerson) => {
   let { name, age } = newPerson;
   const markUp1 = `<li class="Notification__Item--Success">SUCCESS: Person added</li>`;
-  // const markUp2 = `<li class="Notification__Item--Error">${errorMessage}</li>`;
   const markUp3 = `<li class="Notification__Item--Error">ERROR: Name or age is missing</li>`;
 
   if (status && name !== '' && age !== '') {
     elements.addNewNotifList.insertAdjacentHTML('beforeend', markUp1);
   }
-  /*
-  if (!status && errorMessage) {
-    elements.addNewNotifList.insertAdjacentHTML('afterbegin', markUp2);
-  }
-  */
+
   if (name === '' || age === '') {
     elements.addNewNotifList.insertAdjacentHTML('beforeend', markUp3);
   }
