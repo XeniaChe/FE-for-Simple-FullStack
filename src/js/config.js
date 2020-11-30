4b030a3c22891e5e2a28638aa640f7eb744c3b19
Ksenia Troitskaja <xeniacserkun@gmail.com>
2020-11-30 10:58:02 +0100

    11/30 API-URL back to heroku app

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://enigmatic-lake-81712.herokuapp.com/api/users';
-export const API_URL = 'http://localhost:3000/api/users/';
+export const API_URL = 'https://enigmatic-lake-81712.herokuapp.com/api/users';
+// export const API_URL = 'http://localhost:3000/api/users/';
